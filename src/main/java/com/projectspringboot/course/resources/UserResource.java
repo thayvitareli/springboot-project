693b15b4aa33124bed26e310d76f52c3852cebb7
thayvitareli <thainyvitarel@hotmail.com>
2023-06-23 18:55:18 -0300

    Created User and UserResource classes

---
+++ src/main/java/com/projectspringboot/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projectspringboot.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectspringboot.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Thainy","thainy@gmail.com","99313-5224","12345");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
