86d9336d206c6bd4523bbac24a36cc3c62fe5070
thayvitareli <thainyvitarel@hotmail.com>
2023-06-26 20:56:29 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/projectspringboot/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.projectspringboot.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projectspringboot.course.entities.Order;
+
+public interface OrderRepository  extends JpaRepository<Order, Long>{
+
+}
